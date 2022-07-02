4eac397884d0eea2c4461a7edf59becc7d378ab3
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2022-07-02 22:06:31 +0100

    .env file

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
+  const mySetUsername = process.env.USER_NAME;
+  console.log(`My favorite food is ${mySetUsername}`);
   while(true) {
     console.log('Containers rule!');
     await sleep(5000);
